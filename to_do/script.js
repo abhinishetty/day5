24a32e4c4ce27c2ef98cf9c19c98ed9d4cd18a49
abhinishetty <168563864+abhinishetty@users.noreply.github.com>
2025-07-18 17:20:07 +0530

    Add files via upload

---
+++ to_do/script.js
@@ -0,0 +1,15 @@
+let input=document.getElementById("inp");
+tasks=[]
+function AddTask(){
+if(input.value!==""){
+    tasks.push(input)
+    input.value=""
+}
+}
+
+function DeleteTask(){
+
+}
+function showList(){
+
+}
